53a306678f2d545fb102fc6de8af3d8dedb9d810
jaesay <jaesay3153@gmail.com>
2025-01-27 14:40:15 +0900

    웹소켓 예제 구현(브로드캐스트)

---
+++ playground/websocket-example/src/main/java/com/example/websocketexample/Greeting.java
@@ -0,0 +1,5 @@
+package com.example.websocketexample;
+
+public record Greeting(String content) {
+
+}
---
+++ playground/websocket-example/src/main/java/com/example/websocketexample/HelloMessage.java
@@ -0,0 +1,5 @@
+package com.example.websocketexample;
+
+public record HelloMessage(String name) {
+
+}
