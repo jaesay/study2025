30cfb4c0ea22a649262e4609aad0ed897318a3a6
jaesay <jaesay3153@gmail.com>
2025-02-04 23:37:21 +0900

    sse-example 프로젝트 생성

---
+++ playground/sse-example/src/test/java/com/example/sseexample/SseExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sseexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
