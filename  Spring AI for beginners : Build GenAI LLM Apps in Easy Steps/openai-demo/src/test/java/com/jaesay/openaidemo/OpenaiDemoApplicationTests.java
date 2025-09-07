c69219046f28dcb3c9b9580c95c1078f6e1de9bf
jaesay <jaesay3153@gmail.com>
2025-09-07 22:31:01 +0900

    chore(project): 초기 프로젝트 설정 및 파일 추가 🔧 - Gradle 빌드 설정 파일 및 wrapper 추가 - 기본 애플리케이션 클래스 및 테스트 클래스 생성 - .gitignore 및 .gitattributes 파일 추가 - application.properties 파일에 애플리케이션 이름 설정

---
+++  Spring AI for beginners : Build GenAI LLM Apps in Easy Steps/openai-demo/src/test/java/com/jaesay/openaidemo/OpenaiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaesay.openaidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenaiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
