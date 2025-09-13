12e983252f4f200634d4c91d74ad231b3af8c04e
jaesay <jaesay3153@gmail.com>
2025-09-13 20:48:14 +0900

    feat(service): OpenAiService에 기본 어드바이저 추가 ✨ - ChatClient 빌더에 기본 메시지 메모리 어드바이저 추가 - 대화 메모리 창을 사용하여 대화 상태 관리

--- Spring_AI_for_beginners-Build_GenAI_LLM_Apps_in Easy Steps/openai-demo/src/main/java/com/jaesay/openaidemo/services/OpenAiService.java
+++ Spring_AI_for_beginners-Build_GenAI_LLM_Apps_in Easy Steps/openai-demo/src/main/java/com/jaesay/openaidemo/services/OpenAiService.java
@@ -1,6 +1,8 @@
 package com.jaesay.openaidemo.services;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.stereotype.Service;
 
@@ -10,7 +12,9 @@ public class OpenAiService {
     private final ChatClient chatClient;
 
     public OpenAiService(ChatClient.Builder builder) {
-        this.chatClient = builder.build();
+        this.chatClient = builder
+            .defaultAdvisors(MessageChatMemoryAdvisor.builder(MessageWindowChatMemory.builder().build()).build())
+            .build();
     }
 
     public ChatResponse generateAnswer(String question) {
