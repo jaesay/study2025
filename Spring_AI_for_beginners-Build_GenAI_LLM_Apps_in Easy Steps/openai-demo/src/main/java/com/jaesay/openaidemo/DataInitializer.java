b4f3245e7056c9b14975596225ea33deb78c81e7
jaesay <jaesay3153@gmail.com>
2025-09-27 23:44:01 +0900

    feat(openai-demo): 데이터 초기화 기능 추가 ✨ - job_listings.txt 파일을 읽어 벡터 저장소에 문서 추가 - TextReader와 TokenTextSplitter를 사용하여 데이터 처리

---
+++ Spring_AI_for_beginners-Build_GenAI_LLM_Apps_in Easy Steps/openai-demo/src/main/java/com/jaesay/openaidemo/DataInitializer.java
@@ -0,0 +1,27 @@
+package com.jaesay.openaidemo;
+
+
+import jakarta.annotation.PostConstruct;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer {
+
+    private final VectorStore vectorStore;
+
+    @PostConstruct
+    public void init() {
+        TextReader jobListReader = new TextReader(new ClassPathResource("job_listings.txt"));
+        TokenTextSplitter tokenTextSplitter = new TokenTextSplitter(100, 100, 5, 1000, true);
+        List<Document> documents = tokenTextSplitter.split(jobListReader.get());
+        vectorStore.add(documents);
+    }
+}
