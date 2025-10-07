22cf4cfbaf2abaf1bf3bf90bcfcd186fbfa7cc97
jaesay <jaesay3153@gmail.com>
2025-10-07 21:26:17 +0900

    feat: 레디슨 예제 애플리케이션 및 빌드 설정 추가 ✨
    - 레디슨을 사용한 스프링 부트 애플리케이션 구조 추가
    - Gradle 빌드 설정 및 실행 스크립트 추가
    - 기본 애플리케이션 프로퍼티 설정
    - .gitignore 및 .gitattributes 파일 추가

---
+++ playground/redisson-example/src/main/java/com/jaesay/redissonexample/RedissonExampleApplication.java
@@ -0,0 +1,13 @@
+package com.jaesay.redissonexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedissonExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedissonExampleApplication.class, args);
+    }
+
+}
---
+++ playground/redisson-example/src/test/java/com/jaesay/redissonexample/RedissonExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaesay.redissonexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedissonExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
