d9e7b72e54c8efbac979c900a859dd5bf89da528
jaesay <jaesay3153@gmail.com>
2025-10-08 23:24:34 +0900

    feat: Redis 락 레지스트리 예제 애플리케이션 추가 ✨ - Gradle 빌드 설정 및 의존성 추가 - 기본 애플리케이션 및 테스트 클래스 생성 - .gitignore 및 .gitattributes 파일 추가

---
+++ playground/redis-lock-registry-example/src/main/java/com/jaesay/redislockregistryexample/RedisLockRegistryExampleApplication.java
@@ -0,0 +1,13 @@
+package com.jaesay.redislockregistryexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLockRegistryExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisLockRegistryExampleApplication.class, args);
+    }
+
+}
---
+++ playground/redis-lock-registry-example/src/test/java/com/jaesay/redislockregistryexample/RedisLockRegistryExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaesay.redislockregistryexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLockRegistryExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
