b1679b0b54a442964eb54f58c066d9d6082c7f23
jaesay <jaesay3153@gmail.com>
2025-10-11 06:01:02 +0900

    feat: ailogger 프로젝트 추가 ✨ - Gradle 빌드 설정 파일 및 Wrapper 추가 - 기본 애플리케이션 클래스 및 테스트 클래스 생성 - .gitignore 및 .gitattributes 파일 추가 - application.properties 파일로 애플리케이션 이름 설정

---
+++ playground/ailogger/src/main/java/dev/jaesay/ailogger/AiloggerApplication.java
@@ -0,0 +1,13 @@
+package dev.jaesay.ailogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiloggerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AiloggerApplication.class, args);
+  }
+
+}
---
+++ playground/ailogger/src/test/java/dev/jaesay/ailogger/AiloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jaesay.ailogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiloggerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
